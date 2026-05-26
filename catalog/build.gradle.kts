28c4e071fe4e6d245c852bc62af08e2b52e6bbc2
Achmad Daniel Syahputra <achmad24daniel@gmail.com>
2026-05-26 05:20:38 +0000

    release: 0.2.0-alpha (#33)
    Signed-off-by: Achmad Daniel Syahputra <achmad24daniel@gmail.com>

--- catalog/build.gradle.kts
+++ catalog/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "app.cortena.ui.catalog"
         minSdk = 35
         targetSdk = 37
-        versionCode = 1
-        versionName = "0.1.0-alpha"
+        versionCode = 2
+        versionName = "0.2.0-alpha"
     }
 
     buildTypes {
